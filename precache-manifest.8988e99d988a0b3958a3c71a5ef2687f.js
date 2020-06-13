6035583703c19e1c2c52f81e11966d21caabf38d
wooknick <wooknick@naver.com>
2020-06-14 00:55:40 +0900

    Updates

---
+++ precache-manifest.8988e99d988a0b3958a3c71a5ef2687f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e534b9d331264c00485cb671a8326ca",
+    "url": "/always-festival/index.html"
+  },
+  {
+    "revision": "98db311b699cac7aa84f",
+    "url": "/always-festival/static/css/2.e6808bd0.chunk.css"
+  },
+  {
+    "revision": "98db311b699cac7aa84f",
+    "url": "/always-festival/static/js/2.e65af01f.chunk.js"
+  },
+  {
+    "revision": "30efa1e1cd19d9c6774b40e453f38caf",
+    "url": "/always-festival/static/js/2.e65af01f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "77e1f82864084af4b88a",
+    "url": "/always-festival/static/js/main.9d753478.chunk.js"
+  },
+  {
+    "revision": "0677c53772bb8e437985",
+    "url": "/always-festival/static/js/runtime-main.58563d44.js"
+  }
+]);
