a11ea97760b3651500fb89cbc68d6b54e122f66a
wooknick <wooknick@naver.com>
2020-08-02 05:37:23 +0900

    update fetch lineup logic

--- start.js
+++ start.js
@@ -1,17 +1,17 @@
 require("dotenv").config();
-
 const cron = require("node-cron");
-cron.schedule("10 */6 * * *", checkDB);
-
 const express = require("express");
 const path = require("path");
 const cors = require("cors");
 const wrap = require("express-async-wrap");
-
 const mongoose = require("mongoose");
 const Artists = require("./artists");
 const axios = require("axios");
 
+// const variables
+const LINEUP = { red: [], blue: [] };
+
+// DB Connection
 const db = mongoose.connection;
 db.on("error", console.error);
 db.once("open", () => {
@@ -22,6 +22,46 @@ mongoose.connect(process.env.MONGO_URL, {
   useUnifiedTopology: true,
 });
 
+// Express
+const app = express();
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cors());
+
+app.get("*", (req, res, next) => {
+  let protocol = req.headers["x-forwarded-proto"] || req.protocol;
+  if (req.hostname === "localhost") {
+    next();
+  } else if (protocol === "https") {
+    next();
+  } else {
+    let from = `${protocol}://${req.hostname}${req.url}`;
+    let to = `https://${req.hostname}${req.url}`;
+    console.log(`[${req.method}]: ${from} -> ${to}`);
+    res.redirect(to);
+  }
+});
+
+app.use(express.static(path.join(__dirname, "build")));
+// app.get("/api/artists/:stage", wrap(getArtists));
+app.get("/api/artists/:stage", (req, res) => {
+  const stage = req.params.stage;
+  res.json(LINEUP[stage]);
+});
+app.get("/*", (req, res) => {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
+});
+app.listen(process.env.PORT);
+console.log(`server start on http://localhost:${process.env.PORT}`);
+
+// Init
+fetchLineup();
+
+// Cron Schedule
+cron.schedule("0 0 * * *", fetchLineup);
+cron.schedule("10 */6 * * *", checkDB);
+
+// Functions
 async function checkDB() {
   console.log(`DB Check Start : ${new Date()}`);
   try {
@@ -83,7 +123,31 @@ async function checkArtist(artist) {
   }
 }
 
-const getArtists = async (req, res) => {
+async function fetchLineup() {
+  console.log(`fetch Lineup : ${new Date()}`);
+  await Artists.find({ stage: "red" }, (err, artists) => {
+    if (err) {
+      console.log(err);
+    }
+    if (!artists) {
+      console.log("no artist");
+    }
+    const red = artists.sort(() => Math.random() - Math.random()).slice(0, 6);
+    LINEUP.red = red;
+  });
+  await Artists.find({ stage: "blue" }, (err, artists) => {
+    if (err) {
+      console.log(err);
+    }
+    if (!artists) {
+      console.log("no artist");
+    }
+    const blue = artists.sort(() => Math.random() - Math.random()).slice(0, 6);
+    LINEUP.blue = blue;
+  });
+}
+
+async function getArtists(req, res) {
   const stage = req.params.stage;
   await Artists.find({ stage }, (err, artists) => {
     if (err) {
@@ -95,33 +159,4 @@ const getArtists = async (req, res) => {
     const ret = artists.sort(() => Math.random() - Math.random()).slice(0, 6);
     res.json(ret);
   });
-};
-
-const app = express();
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-app.use(cors());
-
-app.get("*", (req, res, next) => {
-  let protocol = req.headers["x-forwarded-proto"] || req.protocol;
-  if (req.hostname === "localhost") {
-    next();
-  } else if (protocol === "https") {
-    next();
-  } else {
-    let from = `${protocol}://${req.hostname}${req.url}`;
-    let to = `https://${req.hostname}${req.url}`;
-    console.log(`[${req.method}]: ${from} -> ${to}`);
-    res.redirect(to);
-  }
-});
-
-app.use(express.static(path.join(__dirname, "build")));
-app.get("/api/artists/:stage", wrap(getArtists));
-app.get("/*", (req, res) => {
-  res.sendFile(path.join(__dirname, "build", "index.html"));
-});
-
-app.listen(process.env.PORT);
-
-console.log(`server start on http://localhost:${process.env.PORT}`);
+}
