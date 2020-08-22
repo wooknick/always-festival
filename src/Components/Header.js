a9309aa18fa8f478849e0f81d661a35b2b06239e
wooknick <wooknick@naver.com>
2020-08-22 22:27:21 +0900

    code refactoring(separate slider from header)

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,9 +1,9 @@
 import React, { useEffect, useState } from "react";
-import styled, { keyframes } from "styled-components";
+import styled from "styled-components";
 import { withRouter } from "react-router-dom";
 import { useMediaQuery } from "react-responsive";
 import axios from "axios";
-import { Instagram } from "./Icons";
+import Slider from "./Slider";
 
 const Header = styled.header`
   width: 100%;
@@ -26,7 +26,7 @@ const Menu = styled.div`
   justify-content: flex-start;
   align-items: center;
   margin-left: 0.3em;
-  div:first-child {
+  div.burger {
     display: flex;
     justify-content: center;
     align-items: center;
@@ -56,95 +56,6 @@ const Menu = styled.div`
         content: " ";
       }
     }
-  }
-`;
-
-const Slider = styled.div`
-  z-index: 10;
-  position: absolute;
-  width: 100%;
-  height: 100%;
-  background-color: rgba(1, 1, 1, 0.5);
-`;
-
-const slideIn = keyframes`
-  from {
-    transform: translateX(-100%);
-  }
-  to {
-    transform: translateX(0%);
-  }
-`;
-
-const slideOut = keyframes`
-  from {
-    transform: translateX(0%);
-  }
-  to {
-    transform: translateX(-100%);
-  }
-`;
-
-const SliderWrapper = styled.div`
-  position: absolute;
-  width: 20rem;
-  height: 100%;
-  background-color: ${(props) =>
-    props.stage === "red"
-      ? props.theme.color.mainRed
-      : props.theme.color.mainBlue};
-  padding: 3rem 2rem 1rem 2rem;
-  animation: ${(props) => (props.open ? slideIn : slideOut)} 0.3s linear;
-  font-size: 2rem;
-  display: flex;
-  flex-direction: column;
-  hr {
-    margin-top: 0em;
-    margin-bottom: 0.6em;
-  }
-`;
-
-const SliderItem = styled.div`
-  width: 100%;
-  height: 1.2em;
-  display: flex;
-  justify-content: flex-end;
-  align-items: center;
-  font-size: 2rem;
-  margin-bottom: 0.6em;
-  color: white;
-  font-family: Varietee;
-  &:hover {
-    cursor: pointer;
-  }
-`;
-
-const SliderFooter = styled.div`
-  width: 100%;
-  min-height: 1.2em;
-  flex: 1;
-  color: white;
-  font-family: Mont;
-  display: flex;
-  flex-direction: column;
-  justify-content: flex-end;
-  div.social {
-    width: 100%;
-    display: flex;
-    justify-content: center;
-    margin-bottom: 1rem;
-    svg {
-      width: 2.5rem;
-      height: 2.5rem;
-      fill: white;
-      &:hover {
-        cursor: pointer;
-      }
-    }
-  }
-  div.copyright {
-    font-size: 0.7rem;
-    text-align: center;
   }
 `;
 
@@ -292,7 +203,7 @@ export default withRouter(({ history, match }) => {
               setIsSliderOpen(true);
             }}
           >
-            <div>|||</div>
+            <div className="burger">|||</div>
           </Menu>
           <Logo stage={stage}>
             <div className="main">
@@ -309,6 +220,7 @@ export default withRouter(({ history, match }) => {
                 setIsSliderOpen(true);
                 setIsDropdownOpen(false);
               }}
+              className="burger"
             >
               |||
             </div>
@@ -347,67 +259,11 @@ export default withRouter(({ history, match }) => {
       )}
       {isSliderOpen && (
         <Slider
-          onClick={() => {
-            setIsSliderOpen(false);
-          }}
-        >
-          <SliderWrapper
-            open={isSliderOpen}
-            stage={stage}
-            onClick={(e) => {
-              e.stopPropagation();
-            }}
-          >
-            <SliderItem
-              onClick={() => {
-                moveTo("/");
-              }}
-            >
-              Entrance
-            </SliderItem>
-            <div>
-              <hr />
-            </div>
-            <SliderItem
-              onClick={() => {
-                moveTo("/stage/red");
-              }}
-            >
-              Red Stage
-            </SliderItem>
-            <SliderItem
-              onClick={() => {
-                moveTo("/stage/blue");
-              }}
-            >
-              Blue Stage
-            </SliderItem>
-            <div>
-              <hr />
-            </div>
-            <SliderItem
-              onClick={() => {
-                moveTo("/info");
-              }}
-            >
-              Information
-            </SliderItem>
-            <SliderFooter>
-              <div className="social">
-                <a
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  href="https://www.instagram.com/alwaysfestival/"
-                >
-                  <Instagram />
-                </a>
-              </div>
-              <div className="copyright">
-                &copy; 2020. AlwaysFestival all rights reserved.
-              </div>
-            </SliderFooter>
-          </SliderWrapper>
-        </Slider>
+          moveTo={moveTo}
+          stage={stage}
+          isSliderOpen={isSliderOpen}
+          setIsSliderOpen={setIsSliderOpen}
+        />
       )}
     </>
   );
---
+++ src/Components/Slider.js
@@ -0,0 +1,161 @@
+import React from "react";
+import styled, { keyframes } from "styled-components";
+import { Instagram } from "./Icons";
+
+const Wrapper = styled.div`
+  z-index: 10;
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(1, 1, 1, 0.5);
+`;
+
+const slideIn = keyframes`
+  from {
+    transform: translateX(-100%);
+  }
+  to {
+    transform: translateX(0%);
+  }
+`;
+
+const slideOut = keyframes`
+  from {
+    transform: translateX(0%);
+  }
+  to {
+    transform: translateX(-100%);
+  }
+`;
+
+const SliderWrapper = styled.div`
+  position: absolute;
+  width: 20rem;
+  height: 100%;
+  background-color: ${(props) =>
+    props.stage === "red"
+      ? props.theme.color.mainRed
+      : props.theme.color.mainBlue};
+  padding: 3rem 2rem 1rem 2rem;
+  animation: ${(props) => (props.open ? slideIn : slideOut)} 0.3s linear;
+  font-size: 2rem;
+  display: flex;
+  flex-direction: column;
+  hr {
+    margin-top: 0em;
+    margin-bottom: 0.6em;
+  }
+`;
+
+const SliderItem = styled.div`
+  width: 100%;
+  height: 1.2em;
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+  font-size: 2rem;
+  margin-bottom: 0.6em;
+  color: white;
+  font-family: Varietee;
+  &:hover {
+    cursor: pointer;
+  }
+`;
+
+const SliderFooter = styled.div`
+  width: 100%;
+  min-height: 1.2em;
+  flex: 1;
+  color: white;
+  font-family: Mont;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-end;
+  div.social {
+    width: 100%;
+    display: flex;
+    justify-content: center;
+    margin-bottom: 1rem;
+    svg {
+      width: 2.5rem;
+      height: 2.5rem;
+      fill: white;
+      &:hover {
+        cursor: pointer;
+      }
+    }
+  }
+  div.copyright {
+    font-size: 0.7rem;
+    text-align: center;
+  }
+`;
+
+const Slider = ({ moveTo, stage, isSliderOpen, setIsSliderOpen }) => {
+  return (
+    <Wrapper
+      onClick={() => {
+        setIsSliderOpen(false);
+      }}
+    >
+      <SliderWrapper
+        open={isSliderOpen}
+        stage={stage}
+        onClick={(e) => {
+          e.stopPropagation();
+        }}
+      >
+        <SliderItem
+          onClick={() => {
+            moveTo("/");
+          }}
+        >
+          Entrance
+        </SliderItem>
+        <div>
+          <hr />
+        </div>
+        <SliderItem
+          onClick={() => {
+            moveTo("/stage/red");
+          }}
+        >
+          Red Stage
+        </SliderItem>
+        <SliderItem
+          onClick={() => {
+            moveTo("/stage/blue");
+          }}
+        >
+          Blue Stage
+        </SliderItem>
+        <div>
+          <hr />
+        </div>
+        <SliderItem
+          onClick={() => {
+            moveTo("/info");
+          }}
+        >
+          Information
+        </SliderItem>
+        <SliderFooter>
+          <div className="social">
+            <a
+              target="_blank"
+              rel="noopener noreferrer"
+              href="https://www.instagram.com/alwaysfestival/"
+            >
+              <Instagram />
+            </a>
+          </div>
+          <div className="copyright">
+            &copy; 2020. AlwaysFestival all rights reserved.
+          </div>
+        </SliderFooter>
+      </SliderWrapper>
+    </Wrapper>
+  );
+};
+
+export default Slider;
